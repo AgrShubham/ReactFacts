8137d1d51893d6d187721cd1be8ab30757587a12
Shubham Agrawal <ag.shubham747@gmail.com>
2023-07-12 12:01:24 +0000

    try 1 to fix logo issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react"
 export default function Navbar(){
     return(
         <nav>
-            <img className="nav-logo" src="src/assets/react.svg" alt="react logo" />
+            <img className="nav-logo" src="./src/assets/react.svg" alt="react logo" />
             <h3 className="nav-head">ReactFacts</h3>
             <h4 className="nav-title">React Course - Project 1</h4>
         </nav>
